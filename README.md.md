76dd17847041cf0f98e34a09044d1f7505488a80
Nawe1337 <nhlfrancia@gmail.com>
2024-04-11 12:44:14 -0300

    Hola Mundo java

---
+++ README.md.md
@@ -0,0 +1,3 @@
+# Bienvenidos!
+**Mi nombre es nahuel francia y a continuacion estaran distintos ejercicios en java, python y js de la facultad**
+*trabajaremos con distintos IDE, tales como, VScode para js, Pycharm para python y NetBeanse para java.*
---
+++ java/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,30 @@
+
+package holamundo;
+
+/**
+ *
+ * @author nahue
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println(" Hola mundo desde java ");
+        
+        int num = 10;
+        System.out.println(num);
+      int aux =  num - 5;
+        System.out.println(aux);
+        
+        //string//
+        
+        String frase = "Bienvenidos";
+        System.out.println(frase);
+        frase = "sigamos creciendo en programacion";
+        System.out.println(frase);
+    }
+    
+}
